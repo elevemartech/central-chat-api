e8ae1dd72f1a9e98799cfad412100f7feba8c9a9
Dener Pires <elevemartech@gmail.com>
2026-03-16 14:40:22 -0300

    up

--- webhooks/urls.py
+++ webhooks/urls.py
@@ -3,4 +3,5 @@ from .views import UazapiWebhookView
 
 urlpatterns = [
     path("webhook/uazapi/", UazapiWebhookView.as_view(), name="uazapi-webhook"),
+    path("webhook/uazapi", UazapiWebhookView.as_view(), name="uazapi-webhook-noslash"),
 ]
