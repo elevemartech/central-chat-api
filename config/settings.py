3e40b3c9a6f8e1bd61c8c75a97e3f87876b32ff0
Dener Pires <elevemartech@gmail.com>
2026-03-16 18:04:19 -0300

    upp

--- config/settings.py
+++ config/settings.py
@@ -33,9 +33,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-     "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.security.SecurityMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
+    "corsheaders.middleware.CorsMiddleware", 
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
